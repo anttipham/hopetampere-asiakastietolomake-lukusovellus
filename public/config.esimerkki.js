4f24896fac6ac46233209786eed3dd0c79ba9ef2
hope <hope@localhost.localdomain>
2020-12-07 10:04:28 +0200

    rename. tekstiä muutettu

--- public/config.esimerkki.js
+++ public/config.esimerkki.js
@@ -1,6 +1,6 @@
 // Heittomerkit alla kuuluvat mukaan. Laita tiedot heittomerkkien keskelle.
 const config = {
-  sheet_id: 'Sheetsin URL-osoitteesta pitkä id tähän. Se voi näyttää esimerkiksi tällaiselta: "4lmlIWQN2x4E2j9nkD2SHhiQXCVmiwzmLmPhSrqlem54"',
+  sheet_id: 'Tähän Sheetsin ID URL-osoitteesta (se pising sotku kauttaviivojen välissä). Se voi näyttää esimerkiksi tällaiselta: "4lmlIWQN2x4E2j9nkD2SHhiQXCVmiwzmLmPhSrqlem54"',
   client_email: 'Service Accountin sähköposti tähän',
   private_key: 'Sairaan pitkä salasana tähän'
 }
