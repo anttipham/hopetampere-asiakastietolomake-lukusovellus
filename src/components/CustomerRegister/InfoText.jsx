20a6a6c23a0ca9ae68a0e5dadd3edf663ad341d2
hope <hope@localhost.localdomain>
2020-12-07 12:24:47 +0200

    tekstiä muutettu

--- src/components/CustomerRegister/InfoText.jsx
+++ src/components/CustomerRegister/InfoText.jsx
@@ -7,13 +7,13 @@ const Header = () => {
           Tässä näkymässä voit katsoa asiakkaita asiakasrekisteristä.
       </p>
       <p>
-          Tulostusnappi tulostaa kaikki asiakkaat, jotka ovat avattuja – eli näet koko asiakkaan kaikki tiedot –
+          Tulostusnappi tulostaa kaikki asiakkaat, jotka ovat avattuja – eli klikattu kerran auki –
           ja näkyvillä jossain kohtaa sivua.
       </p>
       <p>
           Alla oleva filtterikenttä käy kaikki asiakkaat läpi kaikista kentistä.
           Se käy muun muassa syntymäpäivä-, syntymävuosi-, huomioitavaa- ja jopa Ilves vai Tappara -kentän läpi.
-          Iästä ei ole kenttää, joten filtteri ei käy niitä läpi.
+          Iästä ei ole kenttää, joten filtteri ei käy sen kenttiä läpi.
       </p>
     </div>
   )
