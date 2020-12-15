5ac36c290017f7925285c76b1aaa4b22b8db6d74
hope <hope@localhost.localdomain>
2020-12-15 10:54:42 +0200

    margariinia muutettu

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ const { default: styled } = require('styled-components')
 
 const Button = styled.button`
   padding: 5px 8px;
-  margin: 5px;
+  margin: 10px 3px;
   border-radius: 10px;
   border-width: 1px;
   background-color: rgb(245, 245, 245);
